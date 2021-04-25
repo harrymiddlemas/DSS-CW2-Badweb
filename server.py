f36c828a3d5a76fab9f141633f75669f27237e88
Adam <bhe18sru@uea.ac.uk>
2021-04-25 18:06:04 +0100

    added endpoint so using new user_path_id to access user profiles when clicking their name however you cannot get there by typing in their ID into the url

--- server.py
+++ server.py
@@ -3,12 +3,13 @@ import sqlite3
 import encoder
 from functools import wraps
 
-from flask import Flask,g,render_template,redirect,request,session,url_for
+from flask import Flask, g, render_template, redirect, request, session, url_for
 
 app = Flask(__name__)
 app.secret_key = 'thisisabadsecretkey'
 
 DATABASE = 'database.sqlite'
+
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -23,24 +24,28 @@ def get_db():
 
     return db
 
+
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+
 def std_context(f):
     @wraps(f)
-    def wrapper(*args,**kwargs):
-        context={}
+    def wrapper(*args, **kwargs):
+        context = {}
         request.context = context
         if 'userid' in session:
             context['loggedin'] = True
             context['username'] = session['username']
         else:
             context['loggedin'] = False
-        return f(*args,**kwargs)
+        return f(*args, **kwargs)
+
     return wrapper
+
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -48,32 +53,35 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+
 @app.route("/")
 @std_context
 def index():
-    posts = query_db('SELECT posts.creator,posts.date,posts.title,posts.content,users.name,users.username FROM posts JOIN users ON posts.creator=users.userid ORDER BY date DESC LIMIT 10')
+    posts = query_db(
+        'SELECT posts.creator,posts.date,posts.title,posts.content,users.name,users.username,users.USER_PATH_ID FROM posts JOIN users ON posts.creator=users.userid ORDER BY date DESC LIMIT 10')
 
     def fix(item):
         item['date'] = datetime.datetime.fromtimestamp(item['date']).strftime('%Y-%m-%d %H:%M')
-        item['content'] = '%s...'%(item['content'][:200])
+        item['content'] = '%s...' % (item['content'][:200])
         return item
 
     context = request.context
+
     context['posts'] = map(fix, encoder.encode_qry(posts))
     return render_template('index.html', **context)
+
 
 @app.route("/<uname>/")
 @std_context
 def users_posts(uname=None):
-    cid = query_db('SELECT userid FROM users WHERE username="%s"'%(uname))
-    if len(cid)<1:
+    cid = query_db('SELECT userid FROM users WHERE username="%s"' % (uname))
+    if len(cid) < 1:
         return 'No such user'
 
     cid = cid[0]['userid']
 
     if 'userid' in session.keys() and session['userid'] == cid:
-
-        query = 'SELECT date,title,content FROM posts WHERE creator=%s ORDER BY date DESC'%(cid)
+        query = 'SELECT date,title,content FROM posts WHERE creator=%s ORDER BY date DESC' % (cid)
 
         context = request.context
 
@@ -86,46 +94,71 @@ def users_posts(uname=None):
         return render_template('user_posts.html', **context)
     return 'Access Denied'
 
+
+@app.route("/user_path_id/<user_path_id>/")
+@std_context
+def users_posts_by_user_path_id(user_path_id=None):
+    cid = query_db('SELECT userid FROM users WHERE USER_PATH_ID=%s' % (user_path_id))
+    if len(cid) < 1:
+        return 'No such user'
+
+    query = 'SELECT date,title,content, USER_PATH_ID FROM POSTS NATURAL JOIN USERS WHERE USER_PATH_ID=%s ORDER BY ' \
+            'date DESC' % user_path_id
+
+    context = request.context
+
+    def fix(item):
+        item['date'] = datetime.datetime.fromtimestamp(item['date']).strftime('%Y-%m-%d %H:%M')
+        return item
+
+    results = query_db(query)
+    context['posts'] = map(fix, encoder.encode_qry(results))
+    return render_template('user_posts.html', **context)
+
+
 @app.route("/login/", methods=['GET', 'POST'])
 @std_context
 def login():
-    username = request.form.get('username','')
-    password = request.form.get('password','')
+    username = request.form.get('username', '')
+    password = request.form.get('password', '')
     context = request.context
 
-    if len(username)<1 and len(password)<1:
+    if len(username) < 1 and len(password) < 1:
         return render_template('login.html', **context)
 
-    query = "SELECT userid FROM users WHERE username='%s'"%(username)
+    query = "SELECT userid FROM users WHERE username='%s'" % (username)
     account = query_db(query)
-    user_exists = len(account)>0
+    user_exists = len(account) > 0
 
-    query = "SELECT userid FROM users WHERE username='%s' AND password='%s'"%(username, password)
+    query = "SELECT userid FROM users WHERE username='%s' AND password='%s'" % (username, password)
     print(query)
     account2 = query_db(query)
     print(account)
-    pass_match = len(account2)>0
+    pass_match = len(account2) > 0
 
     if user_exists and pass_match:
-            session['userid'] = account[0]['userid']
-            session['username'] = username
-            return redirect(url_for('index'))
+        session['userid'] = account[0]['userid']
+        session['username'] = username
+        return redirect(url_for('index'))
     else:
         # Username or password incorrect
         return redirect(url_for('login_fail', error='Username or password incorrect'))
+
 
 @app.route("/loginfail/")
 @std_context
 def login_fail():
     context = request.context
-    context['error_msg'] = request.args.get('error','Unknown error')
-    return render_template('login_fail.html',**context)
+    context['error_msg'] = request.args.get('error', 'Unknown error')
+    return render_template('login_fail.html', **context)
+
 
 @app.route("/logout/")
 def logout():
     session.pop('userid', None)
     session.pop('username', None)
     return redirect('/')
+
 
 @app.route("/post/", methods=['GET', 'POST'])
 @std_context
@@ -137,31 +170,34 @@ def new_post():
     print(userid)
     context = request.context
 
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('new_post.html', **context)
 
     date = datetime.datetime.now().timestamp()
     title = request.form.get('title')
     content = request.form.get('content')
 
-    query = "INSERT INTO posts (creator, date, title, content) VALUES ('%s',%d,'%s','%s')"%(userid, date, title, content)
+    query = "INSERT INTO posts (creator, date, title, content) VALUES ('%s',%d,'%s','%s')" % (
+        userid, date, title, content)
     insert = query_db(query)
 
     get_db().commit()
 
     return redirect('/')
 
+
 @app.route("/reset/", methods=['GET', 'POST'])
 @std_context
 def reset():
     context = request.context
 
-    email = request.form.get('email','')
-    if email=='':
+    email = request.form.get('email', '')
+    if email == '':
         return render_template('reset_request.html')
     context['email'] = encoder.encode(email)
 
     return render_template('sent_reset.html', **context)
+
 
 @app.route("/search/")
 @std_context
@@ -169,12 +205,12 @@ def search_page():
     context = request.context
     search = request.args.get('s', '')
 
-    #query = "SELECT posts.creator,posts.title,posts.content,users.username FROM posts JOIN users ON posts.creator=users.userid WHERE users.username LIKE '%%%s%%' ORDER BY date DESC LIMIT 10;"%(search)
-    query = "SELECT username FROM users WHERE username LIKE '%%%s%%';"%(search)
+    # query = "SELECT posts.creator,posts.title,posts.content,users.username FROM posts JOIN users ON posts.creator=users.userid WHERE users.username LIKE '%%%s%%' ORDER BY date DESC LIMIT 10;"%(search)
+    query = "SELECT username FROM users WHERE username LIKE '%%%s%%';" % (search)
     users = query_db(query)
 
-    #for user in users:
-        #post['content'] = '%s...'%(post['content'][:50])
+    # for user in users:
+    # post['content'] = '%s...'%(post['content'][:50])
     context['users'] = encoder.encode_qry(users)
     context['query'] = encoder.encode(search)
     return render_template('search_results.html', **context)
@@ -183,13 +219,14 @@ def search_page():
 # Todo: remove or protect reset token
 @app.route("/resetdb/<token>")
 def resetdb(token=None):
-    if token=='secret42':
+    if token == 'secret42':
         import create_db
         create_db.delete_db()
         create_db.create()
         return 'Database reset'
     else:
-        return 'Nope',401
+        return 'Nope', 401
+
 
 if __name__ == '__main__':
     app.run()

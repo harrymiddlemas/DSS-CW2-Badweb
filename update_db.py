678b3110d9a150bde06b191392f7a8a243740260
Benlewis9000 <benjaminlewis9000@gmail.com>
2021-05-03 23:42:19 +0100

    update_db.py fixed

--- update_db.py
+++ update_db.py
@@ -10,16 +10,17 @@ def generate_random_user_path_id():
         user_path_id += str(random.randint(1, 9))
     return user_path_id
 
+def update():
+    db = sqlite3.connect(DATABASE)
+    c = db.cursor()
+    number_of_users = c.execute('SELECT COUNT(*) FROM USERS').fetchall()[0][0]
+    users = c.execute('SELECT USERID FROM USERS').fetchall()
+    c.execute('ALTER TABLE USERS ADD USER_PATH_ID INTEGER(16)')
+    for i in range(number_of_users):
+        userid = users[i][0]
+        c.execute(
+            'UPDATE USERS SET USER_PATH_ID = ' + str(generate_random_user_path_id()) + ' WHERE USERID = ' + str(
+                userid))
 
-db = sqlite3.connect(DATABASE)
-c = db.cursor()
-number_of_users = c.execute('SELECT COUNT(*) FROM USERS').fetchall()[0][0]
-users = c.execute('SELECT USERID FROM USERS').fetchall()
-c.execute('ALTER TABLE USERS ADD USER_PATH_ID INTEGER(16)')
-for i in range(number_of_users):
-    userid = users[i][0]
-    c.execute(
-        'UPDATE USERS SET USER_PATH_ID = ' + str(generate_random_user_path_id()) + ' WHERE USERID = ' + str(
-            userid))
+    db.commit()
 
-db.commit()
